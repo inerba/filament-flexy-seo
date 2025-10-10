14dea94aa458323adc020a2479f58d0f0eac8e1f
Francesco <inerba@gmail.com>
2025-10-10 18:22:52 +0200

    Add content to <input> component view file

---
+++ resources/views/components/input.blade.php
@@ -0,0 +1,3 @@
+Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit suscipit eaque repudiandae cumque magnam obcaecati!
+Placeat, dolores, praesentium aut labore odio culpa reiciendis repellendus, quaerat saepe nam ratione architecto
+repudiandae.
