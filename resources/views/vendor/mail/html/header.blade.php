c57efbc2ffaecdfe16525ecb8727b6f68a690660
Francesco <inerba@gmail.com>
2025-10-24 10:59:44 +0200

    lingua

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,11 +2,12 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
-@if (trim($slot) === 'Laravel')
+{{-- @if (trim($slot) === 'Laravel')
 <img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Laravel Logo">
 @else
 {!! $slot !!}
-@endif
+@endif --}}
+<img src="https://cluster-a.com/images/logo.png" class="logo" alt="CLUSTER-A">
 </a>
 </td>
 </tr>
