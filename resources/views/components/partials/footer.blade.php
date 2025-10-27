333ee6cba6d67d9c858e7ad5b284c321834cba06
Francesco <inerba@gmail.com>
2025-10-27 17:11:19 +0100

    Improve footer layout for better structure

--- resources/views/components/partials/footer.blade.php
+++ resources/views/components/partials/footer.blade.php
@@ -1,6 +1,6 @@
 <div class="text-gray-600 bg-stone-100">
-    <div class="grid grid-cols-4 lg:grid-cols-8 gap-4 lg:gap-24 py-6 lg:py-24 mx-auto max-w-7xl px-4">
-        <div class="col-span-1 ">
+    <div class="grid grid-cols-4 lg:grid-cols-5 gap-4 lg:gap-24 py-6 lg:py-24 mx-auto max-w-7xl px-4">
+        <div class="col-span-1">
             <img src="/images/logo.png" alt="Logo Cluster-A"
                 class="w-full md:w-52 hover:animate-[spin_1500ms_ease-in-out_1_reverse]">
             <h3 class="mt-4 font-display!">Seguici su</h3>
@@ -13,19 +13,20 @@
                     </a>
                 @endforeach
             </div>
+        </div>
 
+        <div class="grid lg:grid-cols-3 col-span-3 gap-4">
+            <div class="text-xs md:text-sm prose prose-p:mb-1 prose-p:mt-0 prose-headings:font-display!">
+                @db_config('website.footer_col_1.' . app()->getLocale())
+            </div>
+            <div class="text-xs md:text-sm prose prose-p:mb-1 prose-p:mt-0 prose-headings:font-display!">
+                @db_config('website.footer_col_2.' . app()->getLocale())
+            </div>
+            <div class="text-xs md:text-sm prose prose-p:mb-1 prose-p:mt-0 prose-headings:font-display!">
+                @db_config('website.footer_col_3.' . app()->getLocale())
+            </div>
         </div>
-        <div
-            class="col-span-3 md:col-span-2 text-xs md:text-base prose prose-p:mb-1 prose-p:mt-0 prose-headings:font-display!">
-            @db_config('website.footer_col_1.' . app()->getLocale())
-        </div>
-        <div class="col-span-2 text-xs md:text-base prose prose-p:mb-1 prose-p:mt-0 prose-headings:font-display!">
-            @db_config('website.footer_col_2.' . app()->getLocale())
-        </div>
-        <div class="col-span-2 text-xs md:text-base prose prose-p:mb-1 prose-p:mt-0 prose-headings:font-display!">
-            @db_config('website.footer_col_3.' . app()->getLocale())
-        </div>
-        <div class="hidden md:block w-full">
+        <div class="col-span-1 hidden md:block bg-red-100">
             <h3 class="mb-4 text-xl font-semibold text-black uppercase font-display!">Sitemap</h3>
             <livewire:cms.menu slug="footer-menu" />
         </div>
